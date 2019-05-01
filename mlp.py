2828c88105050228b80e2e8964c98af57f84db04
OsosKP <ososkp@gmail.com>
2019-05-01 09:45:01 +0100

    Bugfixing. Learns on single input-output groups but struggles to learn (1, 1) in XOR

--- mlp.py
+++ mlp.py
@@ -3,7 +3,7 @@ from math import exp, pow
 
 
 class MultiLayerPerceptron:
-    def __init__(self, num_inputs, num_hidden, num_outputs):
+    def __init__(self, num_inputs, num_hidden, num_outputs, activation_type):
         self.number_inputs = num_inputs
         self.number_hidden_units = num_hidden
         self.number_outputs = num_outputs
@@ -16,11 +16,29 @@ class MultiLayerPerceptron:
             self.output.shape)
         self.randomize()
 
+        self.activation_type = activation_type
+        self.derivative_output_wrt_activations = {
+            "sigmoid": self.derivative_output_wrt_sigmoid_activations,
+            "relu": self.derivative_output_wrt_relu_activations,
+            "tanh": self.derivative_output_wrt_tanh_activations
+        }
+        self.activation = {
+            "sigmoid": self.sigmoid_activation,
+            "relu": self.relu_activation,
+            "tanh": self.tanh_activation
+        }
+
     def randomize(self):
-        self.weights_lower = np.random.uniform(-0.05, 0.05, size=(
-            self.number_hidden_units, self.number_inputs))
-        self.weights_upper = np.random.uniform(-0.05, 0.05, size=(
-            self.number_outputs, self.number_hidden_units))
+        self.weights_lower = np.random.uniform(-0.2, 0.2, size=(
+            self.number_inputs, self.number_hidden_units))
+        self.weights_upper = np.random.uniform(-0.2, 0.2, size=(
+            self.number_hidden_units, self.number_outputs))
+
+        # self.weights_lower = np.ndarray(shape=(2, 2), dtype=float, buffer=np.array([
+        #     [0.15, 0.25], [0.2, 0.3]]))
+        # self.weights_upper = np.ndarray(shape=(2, 2), dtype=float, buffer=np.array([
+        #     [0.4, 0.5], [0.45, 0.55]]))
+
         self.bias = [0.35, 0.6]
         self.deltas_lower = np.zeros(self.weights_lower.shape)
         self.deltas_upper = np.zeros(self.weights_upper.shape)
@@ -31,16 +49,16 @@ class MultiLayerPerceptron:
         self.inputs = input
         # Net_H
         self.total_net_inputs_lower = np.dot(
-            self.weights_lower, self.inputs) + self.bias[0]
+            self.inputs, self.weights_lower) + self.bias[0]
         # Out_H
         self.activations_lower = np.array(
-            map(lambda x: self.sigmoid_activation(x), self.total_net_inputs_lower))
+            map(lambda x: self.activation[self.activation_type](x), self.total_net_inputs_lower))
         # Net_O
         self.total_net_inputs_upper = np.dot(
-            self.weights_upper, self.activations_lower) + self.bias[1]
+            self.activations_lower, self.weights_upper) + self.bias[1]
         # Out_O
         self.activations_upper = np.array(
-            map(lambda x: self.sigmoid_activation(x), self.total_net_inputs_upper))
+            map(lambda x: self.activation[self.activation_type](x), self.total_net_inputs_upper))
         return self.activations_upper
 
     def calculator_error(self, target):
@@ -56,7 +74,7 @@ class MultiLayerPerceptron:
         self.d_error_wrt_output_upper = self.derivative_error_wrt_output(
             target, self.activations_upper)
         # dO_U / dNet_U
-        self.d_output_wrt_activations_upper = self.derivative_output_wrt_activations(
+        self.d_output_wrt_activations_upper = self.derivative_output_wrt_activations[self.activation_type](
             self.activations_upper)
         # # dNet / DW_L
         self.d_input_wrt_weights_lower = self.activations_lower
@@ -64,9 +82,9 @@ class MultiLayerPerceptron:
         # dE / dW
         self.upper_error_wrt_weight = np.zeros(self.deltas_upper.shape)
 
-        for i in range(0, self.d_output_wrt_activations_upper.size):
-            for j in range(0, self.d_input_wrt_weights_lower.size):
-                self.upper_error_wrt_weight[i][j] = self.d_error_wrt_output_upper[i] * \
+        for i in range(self.d_output_wrt_activations_upper.size):
+            for j in range(self.d_input_wrt_weights_lower.size):
+                self.upper_error_wrt_weight[j][i] = self.d_error_wrt_output_upper[i] * \
                     self.d_output_wrt_activations_upper[i] * \
                     self.d_input_wrt_weights_lower[j]
 
@@ -80,16 +98,16 @@ class MultiLayerPerceptron:
         self.d_error_wrt_output_lower = np.sum(np.multiply(
             self.d_error_wrt_activations_upper, self.d_activations_upper_wrt_outputs_lower), 1)
         # dO_L / dNet_L
-        self.d_output_wrt_activations_lower = self.derivative_output_wrt_activations(
+        self.d_output_wrt_activations_lower = self.derivative_output_wrt_activations[self.activation_type](
             self.activations_lower)
         # dNet_L / dW
         self.d_activations_lower_wrt_weights_lower = np.asarray(self.inputs)
         # dE / dW
         self.lower_error_wrt_weight = np.zeros(self.deltas_lower.shape)
 
-        for i in range(0, self.d_error_wrt_output_lower.size):
-            for j in range(0, self.d_activations_lower_wrt_weights_lower.size):
-                self.lower_error_wrt_weight[i][j] = self.d_error_wrt_output_lower[i] * \
+        for i in range(self.d_error_wrt_output_lower.size):
+            for j in range(self.d_activations_lower_wrt_weights_lower.size):
+                self.lower_error_wrt_weight[j][i] = self.d_error_wrt_output_lower[i] * \
                     self.d_output_wrt_activations_lower[i] * \
                     self.d_activations_lower_wrt_weights_lower[j]
 
@@ -100,6 +118,10 @@ class MultiLayerPerceptron:
         for i in range(self.deltas_lower.shape[0]):
             for j in range(self.deltas_lower.shape[1]):
                 self.deltas_lower[i][j] += self.lower_error_wrt_weight[i][j]
+
+        # print('Input: {0}\tOutput: {1}\tError: {2}').format(
+            # self.inputs, self.activations_upper, error)
+
         return error
 
     def update_weights(self, learning_rate):
@@ -112,16 +134,32 @@ class MultiLayerPerceptron:
             for j in range(self.deltas_lower.shape[1]):
                 self.weights_lower[i][j] -= learning_rate * \
                     self.deltas_lower[i][j]
-
         self.deltas_upper = np.zeros(self.deltas_upper.shape)
         self.deltas_lower = np.zeros(self.deltas_lower.shape)
 
     def derivative_error_wrt_output(self, target, layer):
         return np.subtract(layer, target)
 
-    def derivative_output_wrt_activations(self, layer):
+    def derivative_output_wrt_sigmoid_activations(self, layer):
         return np.array(map(lambda x: x * (1 - x), layer))
 
+    def derivative_output_wrt_relu_activations(self, layer):
+        values = np.zeros(layer.shape)
+        for index, value in np.ndenumerate(layer):
+            if (value > 0):
+                values[index] = 1
+        return values
+
+    def derivative_output_wrt_tanh_activations(self, layer):
+        return np.array(map(lambda x: 1 - pow(self.tanh_activation(x), 2), layer))
+
     def sigmoid_activation(self, input): return pow(1. + exp(-input), -1.)
+
+    def relu_activation(self, input): return max(0, input)
+
+    def tanh_activation(self, input):
+        e_pos = exp(input)
+        e_neg = exp(-input)
+        return (e_pos - e_neg) / (e_pos + e_neg)
 
     def squared_error(self, output, target): return pow(target - output, 2.)
--- test.py
+++ test.py
@@ -3,15 +3,22 @@ from mlp import MultiLayerPerceptron
 import numpy as np
 from math import exp, pow
 
+number_inputs = 2
 # input = [0.05, 0.1]
+# input = [1, 1]
 input = [[0, 0], [0, 1], [1, 0], [1, 1]]
-# target = np.array([0.01, 0.99])
+number_hidden_units = 2
+number_outputs = 1
 target = np.array([0, 1, 1, 0])
-learning_rate = 0.9
-max_epochs = 100
+# target = np.array([0])
+# target = np.array([0.01, 0.99, .5])
+learning_rate = .2
+max_epochs = 5000
+activation_type = "relu"
 output = np.zeros(target.shape)
 
-nn = MultiLayerPerceptron(2, 8, 1)
+nn = MultiLayerPerceptron(
+    number_inputs, number_hidden_units, number_outputs, activation_type)
 # for epoch in range(max_epochs):
 #     output = nn.forward(input)
 #     error = nn.backward(target)
@@ -25,7 +32,6 @@ for epoch in range(max_epochs):
         output[index] = nn.forward(value)
         error += nn.backward(target[index])
         nn.update_weights(learning_rate)
-    print('Epoch:\t{0}\tError:\t{1}\nOutput:\t{2}').format(
-        epoch, error, output)
+    print('Epoch:\t{0}\tError:\t{1}').format(epoch, error)
 
 print(output)
